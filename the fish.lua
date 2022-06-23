6f1773bca88cb9ec1ea0fa71905542974d0a19a2
bigolthorny <81837321+bigolthorny@users.noreply.github.com>
2022-06-24 07:52:20 +1000

    Create the fish.lua
    I LOVE FISH!!!!!!!

---
+++ the fish.lua
@@ -0,0 +1,2 @@
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+ESP:Toggle(true) ---this is showing all the fish player base in pacific pocean, be caref yl you might crash from all the fishe
